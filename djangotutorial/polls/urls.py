42894f1c78250c60dd5e8cebaa965986ac791a0d
jeph.dev <jeph.dev@icloud.com>
2025-05-11 22:34:21 +0800

    Implement polls app with models, views, and templates for question and choice management

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('detail/<int:question_id>/', views.detail, name='detail'),
+    path('results/<int:question_id>/', views.results, name='results'),
+    path('vote/<int:question_id>/', views.vote, name='vote'),
 ]
